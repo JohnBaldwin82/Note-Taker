c92f48188dca07f5a8470d1870fd0fb0537c1300
John Baldwin <johnbaldwin82@gmail.com>
2023-05-15 21:10:53 -0600

    trying to figure out the router still

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const apiRoute = require('./routes/apiRoute');
-const htmlRoute = require('./routes/htmlRoute');
-const app = express()
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
+const apiRoute = require("./routes/apiRoute");
+const htmlRoute = require("./routes/htmlRoute");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(apiRoute)
-app.use(htmlRoute)
+app.use(apiRoute);
+app.use(htmlRoute);
 
 app.listen(PORT, () => {
-  console.log(`Now listening on this PORT ${PORT}!`)
+  console.log(`Now listening on this PORT ${PORT}!`);
 });
