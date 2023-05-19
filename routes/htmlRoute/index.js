fc874d0f7856f85832b5850307a2f2315fc684e7
John Baldwin <johnbaldwin82@gmail.com>
2023-05-18 18:57:09 -0600

    added comments

--- routes/htmlRoute/index.js
+++ routes/htmlRoute/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const route = require('express').Router()
 
-route.get('/notes', (req, res) => {
+route.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'))
 })
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+// Setting constants for Require, Express, Path and setting out local host port to 3000
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Setting API Routes
 const apiRoute = require("./routes/apiRoute");
 const htmlRoute = require("./routes/htmlRoute");
 
@@ -12,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(apiRoute);
 app.use(htmlRoute);
-
+// setting port to listen and display what port it is listening on.
 app.listen(PORT, () => {
   console.log(`Now listening on this PORT ${PORT}!`);
 });
